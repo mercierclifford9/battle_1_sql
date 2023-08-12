fdfe0ad60346c2198e373cd6aed5dba630203dc8
mercierclifford9 <140321835+mercierclifford9@users.noreply.github.com>
2023-08-12 17:17:40 -0400

    Add files via upload

--- battle.sql
+++ battle.sql
@@ -5,5 +5,13 @@ total float not null,
  discount float not null, 
  sub_total float not null
 );
+INSERT INTO sale(total, discount, sub_total)VALUES(
+130, 30, 100 
+),
+(
+130, 30, 100 
+),(
+130, 30, 100 
+);
 select sum(total) from sale;
 select count(total) from sale;
